3bf87635fbac0344e1d10e16b5050f51745489e2
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-04-13 19:22:11 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/comic_website/", // 👈 Add this line with your repo name
+  base: "/tendocomic/", // 👈 Add this line with your repo name
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
