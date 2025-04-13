45f8fb045f1d5df56863201e92156c1d584b7bd7
AmineBen203 <2022.mbenhmed@suptem.com>
2025-04-13 19:27:10 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/TendoComic/", // 👈 Add this line with your repo name
+  base: "/tendocomic/", // 👈 Add this line with your repo name
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
