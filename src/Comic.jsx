c022d919032223108dc58caf0362799ff62f862c
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-04-13 22:03:48 +0100

    Update Comic.jsx

--- src/Comic.jsx
+++ src/Comic.jsx
@@ -5,7 +5,7 @@ export default function Comic() {
     <div className="max-w-4xl mx-auto px-4 py-8 grid grid-cols-1 sm:grid-cols-2 gap-4">
       <div className="flex flex-col items-center mt-10 space-y-6 px-4">
         {panels.map(i => (
-          <img key={i} src={`/assets/comic/${i}.png`} alt={`Panel ${i}`} className="w-full h-auto rounded shadow" />
+          <img key={i} src={`/assets/comic/${i}.png`} alt={`Panel ${i}`} className="w-full max-w-md mx-auto px-4 sm:px-0"w" />
         ))}
       </div>
     </div>
