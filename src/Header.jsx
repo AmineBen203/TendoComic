e5377fff1c185d40d0563f14db9d81a623b2534b
AmineBen203 <2022.mbenhmed@suptem.com>
2025-04-13 20:57:28 +0100

    Deploy main branch to Pages root

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,20 +2,18 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   const playAudio = () => {
-    const audio = new Audio('/sounds/allo.m4a');
+    const audio = new Audio('/sounds/ufo.mp3'); // must exist in "public/sounds/"
     audio.play();
   };
 
   return (
     <header className="bg-black text-white p-4 flex justify-between items-center border-b border-gray-700">
       <h1 className="text-xl font-bold tracking-widest flex items-center">
-<<<<<<< HEAD
-      <img src="./src/assets/icon/icon.png" alt="Site Logo" className="h-24 w-24 mr-2"/>
-      <a href="/">
-      <span className="text-3xl">TendoComic</span>
-      </a>  
-=======
-        
+        {/* 👇 Clickable logo triggers audio */}
+        <a href="/" onClick={playAudio} className="flex items-center">
+          <img src="/assets/icon/icon.png" alt="Site Logo" className="h-24 w-24 mr-2" />
+          <span className="text-3xl">TendoComic</span>
+        </a>
       </h1>
 
       <nav className="space-x-4 text-sm uppercase">
