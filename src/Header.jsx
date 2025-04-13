68f1db845b7f34161c3f0f42d276ef8f65650f76
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-04-13 20:51:16 +0100

    Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,8 +5,10 @@ export default function Header() {
   return (
     <header className="bg-black text-white p-4 flex justify-between items-center border-b border-gray-700">
       <h1 className="text-xl font-bold tracking-widest flex items-center">
-      <a href="/"><img src="./src/assets/icon/icon.png" alt="Site Logo" className="h-24 w-24 mr-2"/>
-      <span className="text-3xl">TendoComic</span></a>  
+      <img src="./src/assets/icon/icon.png" alt="Site Logo" className="h-24 w-24 mr-2"/>
+      <a href="/">
+      <span className="text-3xl">TendoComic</span>
+      </a>  
       </h1>
       <nav className="space-x-4 text-sm uppercase">
         <Link to="/">Home</Link>
