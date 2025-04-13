b2278e965c20380942daae1b47e0a56e9d54db3b
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-04-13 21:28:44 +0100

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,14 @@
 
 export default function About() {
   return (
-    <div className="max-w-2xl mx-auto mt-10 px-4">
-      <h2 className="text-2xl font-bold">About the Comic</h2>
-      <p className="mt-4 text-gray-400">
-        Josh is just your average language teacher... until the aliens called. Follow his mysterious black-and-white
-        journey as the only human who can understand an extraterrestrial transmission. Will he survive? Will he teach the aliens verbs?
-      </p>
+    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div className="max-w-2xl mx-auto mt-10 px-4">
+        <h2 className="text-2xl font-bold">About the Comic</h2>
+        <p className="mt-4 text-gray-400">
+          Josh is just your average language teacher... until the aliens called. Follow his mysterious black-and-white
+          journey as the only human who can understand an extraterrestrial transmission. Will he survive? Will he teach the aliens verbs?
+        </p>
+      </div>
     </div>
   );
 }
