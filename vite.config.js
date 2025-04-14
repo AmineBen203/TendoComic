0cec650bed5cab0490d1346b6708518f60f66cea
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-04-14 20:58:42 +0000

    Blank screen fix

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
-  plugins: [react({
-    // Add fast refresh options
-    fastRefresh: true,
-    includePaths: ['src/**/*'],
-  })],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100
-    },
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      overlay: true
-    }
-  },
-  clearScreen: false
-});
+  plugins: [react()],
+  base: '/'
+})
