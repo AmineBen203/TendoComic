3b1acff3a66a20ff11d6a1d313606fdefb817ece
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-09-07 14:23:12 +0100

    Updated the comic database access

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zigiuudlksfggexvwepd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppZ2l1dWRsa3NmZ2dleHZ3ZXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NzA2NzUsImV4cCI6MjA2MDI0NjY3NX0.m2TYg18ZJNVqCdDCeCYF-ZvrZgpwwFlL5YSFV6cN7Ng'
+const supabaseUrl = 'https://vaswaejyyniocbvzjdek.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhc3dhZWp5eW5pb2NidnpqZGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNDkzMjAsImV4cCI6MjA3MjgyNTMyMH0.GhBklC1WYRpIQnOdMuJfEipc3CB918LspU1-NSfnXvo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
