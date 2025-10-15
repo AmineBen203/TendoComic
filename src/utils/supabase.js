1633644486deb1cd026b5786d4f9341de62b51ec
AmineBen203 <121176837+AmineBen203@users.noreply.github.com>
2025-10-15 19:39:32 +0000

    oups...

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vaswaejyyniocbvzjdek.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhc3dhZWp5eW5pb2NidnpqZGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNDkzMjAsImV4cCI6MjA3MjgyNTMyMH0.GhBklC1WYRpIQnOdMuJfEipc3CB918LspU1-NSfnXvo'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
